f3241f581d07f8912752739349682de2a5f879dd
Jan Spidlen <jan.spidlen@airbnb.com>
2017-09-16 15:08:31 -0700

    adding testing support for api and android-async-http lib

--- Flicks/app/src/main/java/com/codepath/flicks/MovieActivity.java
+++ Flicks/app/src/main/java/com/codepath/flicks/MovieActivity.java
@@ -2,6 +2,14 @@ package com.codepath.flicks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        Toast.makeText(getApplicationContext(), "geat", Toast.LENGTH_LONG).show();
+        client.get(url, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Toast.makeText(getApplicationContext(), "Succcess", Toast.LENGTH_LONG).show();
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Toast.makeText(getApplicationContext(), "failue", Toast.LENGTH_LONG).show();
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
     }
 }
